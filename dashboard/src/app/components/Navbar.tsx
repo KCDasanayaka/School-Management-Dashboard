e3660972c6ec0da43c3cce9a60973b4819c22bbb
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-11-08 23:52:15 +0530

    updated codes

--- dashboard/src/app/components/Navbar.tsx
+++ dashboard/src/app/components/Navbar.tsx
@@ -1,8 +1,23 @@
+import Image from "next/image"
+import search from "../../../public/search.png"
+import message from "../../../Public/message.png"
+import annoucement from "../../../public/announcement.png"
 
 const Navbar = () => {
   return (
-    <div className=''>
-        Navbar
+    <div className="flex items-center justify-between p-4">
+        <div className="hidden md:flex">
+            <Image src={search} alt="" width={14} height={14}/> 
+            <input type="text" placeholder="Search...." />
+        </div>
+        <div className="classname">
+            <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center">
+                <Image src={message} alt="" w-20 h-20/>
+            </div>
+            <div className="bg-white rounded-full w-7 h-7 flex items-center justify-center">
+                <Image src={annoucement} alt="" w-20 h-20/>
+            </div>
+        </div>
     </div>
   )
 }
--- dashboard/src/app/layout.tsx
+++ dashboard/src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Link from "next/link";
+import Image from "next/image";
+
+// Assuming Menu component is available
+import Menu from "../app/components/Menu"; // Adjust the path as needed
+import Navbar from "./components/Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,12 +24,20 @@ export default function RootLayout({
     <html lang="en">
       <head />
       <body className={inter.className}>
-        <div className="h-screen flex">
-          <div className="w-[8%] lg:w-[16%] xl:w-[14%] bg-red-500">
-            l
+        <div className="h-full flex">
+          {/* Sidebar */}
+          <div className="w-[8%] lg:w-[16%] xl:w-[14%] flex flex-col items-center lg:items-start p-4 bg-white h-full">
+            <Link href="/" className="flex items-center justify-center gap-2 lg:justify-start">
+              <Image src="/logo.png" alt="logo" width={32} height={32} />
+              <span className="hidden lg:block text-white font-semibold">LMS</span>
+            </Link>
+            <Menu/> {/* Render the Menu component here */}
           </div>
-          <div className="w-[92%] lg:w-[84%] xl:w-[86%] bg-blue-600">
-            r
+
+          {/* Main Content Area */}
+          <div className="w-[92%] lg:w-[84%] xl:w-[86%] p-6 bg-[#F7F8Fa] overflow-scroll">
+            <Navbar/>
+            {children}
           </div>
         </div>
       </body>
