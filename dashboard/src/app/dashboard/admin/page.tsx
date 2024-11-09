7c7cb5cb5f636297c6b7570fdab75aa6e3f64f93
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-11-09 23:49:23 +0530

    updated codes

--- dashboard/src/app/dashboard/admin/page.tsx
+++ dashboard/src/app/dashboard/admin/page.tsx
@@ -1,15 +1,27 @@
+import AttendanceChart from "@/app/components/AttendanceChart"
+import CountChart from "@/app/components/CountChart"
 import UserCard from "@/app/components/UserCard"
 
 const AdminPage = () => {
     return (
         <div className="p-4 flex gap-4 flex-col md:flex-row">
-         <div className="w-full lg:w-2/3">
+         <div className="w-full lg:w-2/3 flex flex-col gap-8">
          {/* User Cards */}
          <div className="flex gap-4 justify-between flex-wrap">
             <UserCard type="student"/>
             <UserCard type="teacher"/>
             <UserCard type="parent"/>
             <UserCard type="staf"/>
+         </div>
+         <div className="flex gap-4 flex-col lg:flex-row">
+            <div className="w-full lg:w-1/3 h-[450px]">
+                <CountChart/>
+            </div>
+            <div className="w-full lg:w-2/3 h-[450px]">
+            <AttendanceChart/>
+            </div>
+         </div>
+         <div className="">
 
          </div>
          </div>
