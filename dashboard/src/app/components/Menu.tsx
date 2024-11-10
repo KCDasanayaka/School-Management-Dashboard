4fb1b81266466a8897fab226ba45687a30cdea37
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-11-10 23:54:03 +0530

    updated codes

--- dashboard/src/app/components/Menu.tsx
+++ dashboard/src/app/components/Menu.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { role } from "../lib/data";
 
 const menuItems = [
     {
@@ -124,12 +125,15 @@ const menuItems = [
           <div className="flex flex-col gap-2" key={i.title}>
             <span className="hidden lg:block text-gray-400 font-light my-4">{i.title}</span>
             {i.items.map(item => {
-              return (
-                <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-400 py-2">
+              if(item.visible.includes(role)){
+                return (
+                <Link href={item.href} key={item.label} className="flex items-center justify-center lg:justify-start gap-4 text-gray-400 py-2 md:px-2 rounded-md hover:bg-lamaSkyLight">
                   <Image src={item.icon} alt="" width={20} height={20} className=""/>
                   <span className="hidden lg:block">{item.label}</span>
                 </Link>
               );
+              }
+              
             })}
           </div>
         );
