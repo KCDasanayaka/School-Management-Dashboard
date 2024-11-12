90223577f25a28290c8b7cb9d94cf69773adf7ce
KCDasanayaka <116500659+KCDasanayaka@users.noreply.github.com>
2024-11-12 23:52:24 +0530

    updated codes

--- dashboard/src/app/components/BigCalendar.tsx
+++ dashboard/src/app/components/BigCalendar.tsx
@@ -1,20 +1,18 @@
-"use client";
-import { Calendar, momentLocalizer, Views } from 'react-big-calendar';
-import moment from 'moment';
-import "react-big-calendar/lib/css/react-big-calendar.css";
+"use client"
+import { Calendar, momentLocalizer, View, Views } from 'react-big-calendar'
+import moment from 'moment'
+import "react-big-calendar/lib/css/react-big-calendar.css"
 import { useState } from 'react';
 import { calendarEvents } from '../lib/data';
 
-console.log("Loaded Events:", calendarEvents);
 const localizer = momentLocalizer(moment);
 
 const BigCalendar = () => {
-    const [view, setView] = useState(Views.WORK_WEEK);  // Set initial view to WORK_WEEK
+    const [view, setView] = useState<View>(Views.WORK_WEEK);  // Set initial view to WORK_WEEK
 
-    const handleOnChangeView = (selectedView) => {
+    const handleOnChangeView = (selectedView: View)=>{
         setView(selectedView);
     };
-
     return (
         <div className='text-black'>
             <Calendar
@@ -22,10 +20,10 @@ const BigCalendar = () => {
                 events={calendarEvents}
                 startAccessor="start"
                 endAccessor="end"
-                views={["work_week", "day", "week"]} // Added "week" for testing
+                views={["work_week","day"]}
                 view={view}
-                onView={handleOnChangeView}
                 style={{ height: 500 }}
+                onView={handleOnChangeView}
             />
         </div>
     );
